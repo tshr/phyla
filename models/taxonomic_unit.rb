5d6cc47a9beffa9d433b51d4f5da93e8a78df55f
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-03-26 22:28:35 -0700

    get parent and ancestors methods

--- models/taxonomic_unit.rb
+++ models/taxonomic_unit.rb
@@ -1,2 +1,25 @@
 class TaxonomicUnit < ActiveRecord::Base
+
+  def get_parent
+    parent_tsn ? self.class.find(parent_tsn) : nil
+  end
+
+  def get_parent_id
+    get_parent.id
+  end
+
+  def get_ancestors
+    ancestors = []
+    taxon_unit = self
+
+    while taxon_unit.parent_tsn != 0 do
+      taxon_unit = taxon_unit.get_parent
+      ancestors << taxon_unit
+    end
+    ancestors
+  end
+
+  def get_ancestors_ids
+    get_ancestors.map{|a| a.id}
+  end
 end
