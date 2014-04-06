ccccd72fa36d27901364636eab66550d3484b282
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-04-06 08:00:56 -0700

    commenting unused methods for now

--- models/taxonomic_unit.rb
+++ models/taxonomic_unit.rb
@@ -6,28 +6,28 @@ class TaxonomicUnit < ActiveRecord::Base
     parent_tsn != 0 ? self.class.find(parent_tsn) : nil
   end
 
-  def get_ancestors
-    ancestors = []
-    taxon_unit = self
+  # def get_ancestors
+  #   ancestors = []
+  #   taxon_unit = self
 
-    while taxon_unit.parent_tsn != 0 do
-      taxon_unit = taxon_unit.parent
-      ancestors << taxon_unit
-    end
-    ancestors
-  end
+  #   while taxon_unit.parent_tsn != 0 do
+  #     taxon_unit = taxon_unit.parent
+  #     ancestors << taxon_unit
+  #   end
+  #   ancestors
+  # end
 
-  def get_ancestor_ids
-    get_ancestors.map{|a| a.id}
-  end
+  # def get_ancestor_ids
+  #   get_ancestors.map{|a| a.id}
+  # end
 
-  def common_ancestor(taxon_unit)
-    (get_ancestors & taxon_unit.get_ancestors).first
-  end
+  # def common_ancestor(taxon_unit)
+  #   (get_ancestors & taxon_unit.get_ancestors).first
+  # end
 
-  def is_ancestor_of?(taxon_unit)
-    taxon_unit.get_ancestor_ids.include?(id)
-  end
+  # def is_ancestor_of?(taxon_unit)
+  #   taxon_unit.get_ancestor_ids.include?(id)
+  # end
 
   def get_children
     self.class.where(parent_tsn: tsn)
